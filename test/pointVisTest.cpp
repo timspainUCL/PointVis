0c3a0372878f2ffb2cd4b65a1ce13c8a286ad783
Tim Spain <t.spain@ucl.ac.uk>
2021-01-05 12:47:11 +0000

    Add a test to make sure the points have been added.

--- test/pointVisTest.cpp
+++ test/pointVisTest.cpp
@@ -6,3 +6,31 @@
 TEST_CASE("Trivial test", "[trivial]") {
 	REQUIRE((2+2) == 4);
 }
+
+TEST_CASE("addPoints test", "[pointVisTest]") {
+	std::vector<std::array<double, PointVis::nDim>> points ({
+			{1.0, 1, 1},
+			{-1, 1, 1},
+			{1, -1, 1},
+			{-1, -1, 1},
+			{1, 1, -1},
+			{-1, 1, -1},
+			{1, -1, -1},
+			{-1, -1, -1},
+			{1, 0, 0},
+			{-1, 0, 0},
+			{0, 1, 0},
+			{0, -1, 0},
+			{0, 0, 1},
+			{0, 0, -1}
+	});
+
+	std::vector<std::string> colours({
+		"White"
+	});
+
+	PointVis pv = PointVis();
+	pv.addPoints(points);
+
+	REQUIRE(pv.size() == points.size());
+}
