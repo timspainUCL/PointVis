8c4bb13bf5c8f0467b23c2f5e3d442a624c29440
Tim Spain <t.spain@ucl.ac.uk>
2021-01-05 12:42:05 +0000

    PointsViewer becomes a wrapper to the PointVis library class.

--- src/PointsViewer.h
+++ src/PointsViewer.h
@@ -8,29 +8,10 @@
 #ifndef SRC_POINTSVIEWER_H_
 #define SRC_POINTSVIEWER_H_
 
-#include <vector>
-#include <array>
-#include <string>
+#include "pointVis.hpp"
 
-class PointsViewer {
-public:
-	// Three dimensions
-	static const int nDim = 3;
+class PointsViewer : public PointVis
+{
 
-	PointsViewer();
-//	virtual ~PointsViewer(); // Use default dtor
-
-	void addPoints(std::vector<std::array<double, nDim>>);
-	void setColours(std::vector<std::string>);
-	void setSizes(std::vector<int>);
-	int size();
-	void clearAllPoints();
-
-	int run(); // Run until the window closes
-protected:
-	std::vector<std::array<double, nDim>> points;
-	std::vector<std::string> colours;
-	std::vector<int> sizes;
 };
-
 #endif /* SRC_POINTSVIEWER_H_ */
